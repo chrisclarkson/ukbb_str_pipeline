e7e05dd21525e107f6353fbfd520a511fd77c371
Chris Clarkson <c.clarkson@ucl.ac.uk>
2025-12-02 10:46:15 +0000

    icd10 dating

---
+++ extracting_icd10_codes_for_dating.R
@@ -0,0 +1,26 @@
+library(data.table)
+setwd('~/Downloads')
+# template=read.csv('ARI_for_chris.csv',header=T)[,1:7]
+
+data_old=data.frame(fread('data_500k.tsv.gz',header=T),stringsAsFactors=F)
+data=data[((data$gene=='HTT') & (data$A2>=36)),]
+data_old$VCF=basename(gsub('.vcf','',data_old$VCF))
+
+icds=gsub(" ",'',gsub("\\]","",gsub("\\[","",gsub("'","",do.call('c',lapply(data$p41202_Diagnoses_._main_ICD10,function(x){return(unlist(strsplit(x,',')))}))))))
+icds=icds[order(icds[!is.na(icds)])]
+counts=table(icds)
+icds=names(counts)[counts>=2]
+# icds=unique(icds[order(icds[!is.na(icds)])])
+write.table(icds,'icds_gt36.tsv',row.names = F,col.names = F,quote = F)
+col_order=colnames(meta_data)[grep(pattern='41262',colnames(meta_data))]
+col_order=cbind(col_order,unlist(lapply(col_order,function(x){return(strsplit(x,'._Array_')[[1]][2])})))
+col_order=col_order[order(as.numeric(col_order[,2])),1]
+out=cbind(meta_data$eid,gsub("'","",meta_data$p41202_Diagnoses_._main_ICD10),apply(meta_data[,col_order],2,as.character.Date))
+out[,2]=gsub('\\[','',out[,2])
+out[,2]=gsub('\\]','',out[,2])
+out[,2]=gsub(' ','',out[,2])
+
+colnames(out)[1:2]=c('eid','codes')
+write.table(out,'ukbb_informal_dated_main_icd10_codes.tsv',sep='\t',row.names = F,quote=F)
+
+#then use python parse_icd10_dates.py
